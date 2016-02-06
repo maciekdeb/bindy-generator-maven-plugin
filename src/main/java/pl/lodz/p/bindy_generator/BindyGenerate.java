ca842c2df2d695dea984d233c4b61d0d63b5250a
maciek <maciekdeb@gmail.com>
2016-02-06 18:57:19 +0100

    Initial version of simple mojo configuration for maven plugin implementation

---
+++ src/main/java/pl/lodz/p/bindy_generator/BindyGenerate.java
@@ -0,0 +1,22 @@
+package pl.lodz.p.bindy_generator;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * Goal which touches a timestamp file.
+ *
+ * @deprecated Don't use!
+ */
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+public class BindyGenerate extends AbstractMojo {
+
+    @Parameter(defaultValue = "john", property = "name", required = true)
+    private String name;
+
+    public void execute() throws MojoExecutionException {
+    }
+}
